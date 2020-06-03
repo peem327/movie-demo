8ac01869928f33a31f7bac21d1cba44e10ea442b
Peem <peem327@gmail.com>
2020-06-04 01:18:57 +0700

    update

--- src/helpers.js
+++ src/helpers.js
@@ -4,12 +4,3 @@ export const calcTime = (time) => {
   const mins = time % 60;
   return `${hours}h ${mins}m`;
 }
-// Convert a number to money formatting
-export const convertMoney = (money) => {
-  var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0,
-  });
-  return formatter.format(money);
-}
