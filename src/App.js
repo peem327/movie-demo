17479a28a08456d1381222171323f0c8a73241ae
Peem <peem327@gmail.com>
2020-06-04 12:19:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/elements/Header";
 import NotFound from "./components/elements/NotFound";
 import Home from "./components/Home";
-import Movie from "./components/Home"
+import Movie from "./components/Movie";
 
 
 const App = () => {
